be57d32fb8d052fcbb6ecc6d55348c93eb1d7892
Hemanth Gokavarapu <5942165+hemanthgk10@users.noreply.github.com>
2025-10-03 16:26:35 -0700

    Update aws-v4-s3-sample.tf

--- aws/aws-v4-s3-sample.tf
+++ aws/aws-v4-s3-sample.tf
@@ -6,6 +6,7 @@ resource "aws_s3_bucket" "kwan_example" {
 resource "aws_s3_bucket_acl" "kwan_example" {
   bucket = local.kwan_example_name
   acl    = "public"
+
 }
 
 resource "aws_s3_bucket_versioning" "kwan_example" {
