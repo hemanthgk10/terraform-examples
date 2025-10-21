7406aa8faeb087ed0b3b4cea7db1756381fedb06
Hemanth Gokavarapu <5942165+hemanthgk10@users.noreply.github.com>
2025-10-21 13:18:27 -0700

    Change S3 bucket ACL from private to public

--- aws/aws-v4-s3-sample.tf
+++ aws/aws-v4-s3-sample.tf
@@ -5,7 +5,7 @@ resource "aws_s3_bucket" "kwan_example" {
 
 resource "aws_s3_bucket_acl" "kwan_example" {
   bucket = local.kwan_example_name
-  acl    = "private"
+  acl    = "public"
 }
 
 resource "aws_s3_bucket_versioning" "kwan_example" {
